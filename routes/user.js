a7c8f865916c72890253867a5cbccf39a3aaa2b6
GovindaEkbote <govindaekbote7@gmail.com>
2025-04-20 13:24:42 +0530

    Get all Users

--- routes/user.js
+++ routes/user.js
@@ -114,4 +114,15 @@ router.get('/user/:id',async(req,res) =>{
   })
 })
 
+router.get('/users',async(req,res) =>{
+  const users = await User.find();
+  if (!users) {
+    return res.status(400).json({
+      message: "User Not Found",
+    });
+  }
+  res.status(200).json({
+    users
+  })
+})
 module.exports = router;
