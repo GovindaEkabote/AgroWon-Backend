a753db240cafd725b69e5bba513de72420f55736
GovindaEkbote <govindaekbote7@gmail.com>
2025-05-12 13:15:49 +0530

    Create api to get all cart item

--- routes/cart.js
+++ routes/cart.js
@@ -1,6 +1,19 @@
-const Cart = require('../Models/cart')
-const express = require('express')
+const { json } = require("body-parser");
+const Cart = require("../Models/cart");
+const express = require("express");
 const router = express.Router();
+
+router.get("/", async (req, res) => {
+  try {
+    const cartList = await Cart.find(req.query);
+    if (!cartList) {
+      res.status(500).json({ success: false });
+    }
+    return res.status(200), json(cartList);
+  } catch (error) {
+    res.status(500), json({ success: false });
+  }
+});
 
 
 
